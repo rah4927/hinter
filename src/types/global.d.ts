65d1cfc93797ac6ce9319672312fbb557029ea95
rsaha <rah4927@gmail.com>
2025-03-06 16:11:39 -0500

    Simplify global types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,24 +1,20 @@
-declare global {
-  interface Window {
-    MathJax: {
-      typesetPromise: (elements?: Element[]) => Promise<void>;
-      startup: {
-        ready: () => void;
-        defaultReady: () => void;
-      };
-      tex: {
-        packages: Record<string, string[]>;
-        inlineMath: [string, string][];
-        displayMath: [string, string][];
-        processEscapes: boolean;
-      };
-      options: {
-        skipHtmlTags: string[];
-        ignoreHtmlClass: string;
-        processHtmlClass: string;
-      };
+interface Window {
+  MathJax: {
+    typesetPromise: (elements?: Element[]) => Promise<void>;
+    startup: {
+      ready: () => void;
+      defaultReady: () => void;
     };
-  }
-}
-
-export {}; 
+    tex: {
+      packages: Record<string, string[]>;
+      inlineMath: [string, string][];
+      displayMath: [string, string][];
+      processEscapes: boolean;
+    };
+    options: {
+      skipHtmlTags: string[];
+      ignoreHtmlClass: string;
+      processHtmlClass: string;
+    };
+  };
+} 
